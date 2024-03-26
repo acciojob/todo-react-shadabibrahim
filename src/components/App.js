753882a51ee86696ee25f21538cd9c64a0f9a6a5
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-03-27 01:15:38 +0530

    submitting code-Wed Mar 27 2024 01:15:37 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [task,setTask] = useState(["Coding"]);
+  const [inputTask, setInputTask] = useState("");
+
+  const addTask= ()=>{
+    setTask([...task,inputTask]);
+    setInputTask("");
+  }
+
+  const deleteTask =(taskToBeDeleted)=>{
+    let remainingTask = task.filter((data)=>data!=taskToBeDeleted)
+    setTask(remainingTask);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>To-Do List</h1>
+        <input type="text" 
+          placeholder="Enter Task"
+          value={inputTask}
+          onChange={(e)=>setInputTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Todo</button>
+        <div className="list">
+          <ul>
+          {
+            task.map((item)=>{
+              return(
+                <li>
+                  <span>{item}</span>
+                  <button onClick={()=>deleteTask(item)}>Delete</button>
+                </li>
+              )
+            })
+          }
+          
+          </ul>
+        </div>
+        
     </div>
   )
 }
